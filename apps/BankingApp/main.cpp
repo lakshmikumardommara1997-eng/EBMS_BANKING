f79e82cd58460f5a1616ba35e2289ef562db5e57
lakshmikumardommara1997-eng <lakshmi@HPLKUMAR.localdomain>
2026-08-06 17:36:45 +0000

    implementing connection pool

--- apps/BankingApp/main.cpp
+++ apps/BankingApp/main.cpp
@@ -73,36 +73,65 @@ void ConsumeCustomerData(std::deque<std::unique_ptr<Banking::Customer>>& custome
         }
     }
 }
+void initLogger(Banking::Logger& logger)
+{
+    logger.init();
+}
 
+void intiConnectionPool(Banking::IConnectionPool& connectionPool)
+{
+    Banking::AppConfig& appConfig = Banking::AppConfig::getInstance();
+    if (connectionPool.initialize(appConfig.getDBUser(), appConfig.getDBPassword(), appConfig.getDBConnectString(), 5))
+    {
+        Banking::Logger::getInstance().info("Connection pool initialized successfully", __FILE__, __LINE__, __FUNCTION__);
+    }
+    else
+    {
+        Banking::Logger::getInstance().error("Failed to initialize connection pool", __FILE__, __LINE__, __FUNCTION__);
+    }
+}
 int main()
 {
     Banking::Common common;
     Banking::AppConfig& appConfig = Banking::AppConfig::getInstance();
     appConfig.LoadConfigFromFile("param/Ebms.cfg");
     Banking::Logger& logger = Banking::Logger::getInstance();
-    Banking::Logger::getInstance().init();
-    std::cout << "Application Name: " << appConfig.getAppName() << std::endl;
-    std::cout << "Application Version: " << appConfig.getAppVersion() << std::endl;
-    std::cout << "Configuration File Path: " << appConfig.getConfigFilePath() << std::endl;
+    initLogger(logger);
+    
     logger.info("Starting Banking Application", __FILE__, __LINE__, __FUNCTION__);
     logger.info("Application Configuration: " + appConfig.toString(), __FILE__, __LINE__, __FUNCTION__);
-    common.printWelcome();
+
 
     // Create a new customer
     Banking::Types::CUSTOMER_ID customerId = 1;
     Banking::Customer customer(customerId, "John Doe", "123 Main St", "555-1234", "john.doe@example.com");
     std::deque<std::unique_ptr<Banking::Customer>> customers;
     customers.push_back(std::make_unique<Banking::Customer>(customer));
-    //customers[0]->printInfo();
+   
     logger.info("Customer created: " + customers[0]->toString(), __FILE__, __LINE__, __FUNCTION__);
-    logger.debug("Debugging customer creation", __FILE__, __LINE__, __FUNCTION__);
-    logger.warn("This is a warning message", __FILE__, __LINE__, __FUNCTION__);
-    logger.error("This is an error message", __FILE__, __LINE__, __FUNCTION__);
     std::thread producerThread(ProduceCustomerData, std::ref(customers), 2, 10000,std::ref(logger));
     std::thread consumerThread(ConsumeCustomerData, std::ref(customers), std::ref(logger));
     producerThread.join();
     consumerThread.join();
     std::unique_ptr<Banking::IConnectionPool> connectionPool = std::make_unique<Banking::ConnectionPool>();
+    
+    intiConnectionPool(*connectionPool);
+    std::unique_ptr<Banking::IDatabaseConnection> dbConnection = connectionPool->acquireConnection();
+    if(dbConnection && dbConnection->isConnected())
+    {
+        logger.info("Connected to Oracle database successfully", __FILE__, __LINE__, __FUNCTION__);
+        // Perform database operations here
+        dbConnection->disconnect();
+        logger.info("Disconnected from Oracle database", __FILE__, __LINE__, __FUNCTION__);
+    }
+    else
+    {
+        logger.error("Failed to connect to Oracle database", __FILE__, __LINE__, __FUNCTION__);
+        logger.info("user: " + appConfig.getDBUser() + ", password: " + appConfig.getDBPassword() + ", connect string: " + appConfig.getDBConnectString(), __FILE__, __LINE__, __FUNCTION__);
+    }
+
+    
+    /*
     if (connectionPool->initialize(appConfig.getDBUser(), appConfig.getDBPassword(), appConfig.getDBConnectString(), 5))
     {
         logger.info("Connection pool initialized successfully", __FILE__, __LINE__, __FUNCTION__);
@@ -112,6 +141,9 @@ int main()
         logger.error("Failed to initialize connection pool", __FILE__, __LINE__, __FUNCTION__);
     }
     std::unique_ptr<Banking::IDatabaseConnection> dbConnection = connectionPool->acquireConnection();
+    
+    
+    
     if (dbConnection && dbConnection->isConnected())
     { 
         logger.info("Connected to Oracle database successfully", __FILE__, __LINE__, __FUNCTION__);
@@ -127,6 +159,7 @@ int main()
     }  
     connectionPool->releaseConnection(std::move(dbConnection)); 
     connectionPool->shutdown();
+    */
 
     logger.info("Banking Application finished", __FILE__, __LINE__, __FUNCTION__);
     return 0;
